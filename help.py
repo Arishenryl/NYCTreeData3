7d00773809c549f1fced9250c134d50b0929d77b
Arishenryl <95382595+Arishenryl@users.noreply.github.com>
2023-03-28 17:01:35 -0400

    Update help.py

--- help.py
+++ help.py
@@ -1,4 +1,10 @@
-import  
+import sys
 import 
 
-print "Welcome to the 
+print "Welcome to the Welcome to the treequery program. To begin, try typing 'help' for the list of valid commands."
+
+listtrees = input()
+treeinfo = 
+nearby 
+quit
+
