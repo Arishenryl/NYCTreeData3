6e3feb6d82618d4969a6a670d4500378a2cc88fc
gabesg20 <32378049+gabesg20@users.noreply.github.com>
2023-03-28 17:18:54 -0400

    Update LoadData.py

--- LoadData.py
+++ LoadData.py
@@ -1,3 +1,4 @@
+#Part 1
 import sys
 import pandas as pd
 
@@ -7,3 +8,22 @@ file=str(sys.argv[1])
 #Using pandas library to load CSV file into pandas 'DataFrame'
 Tree_data= pd.read_csv(file)
 
+#Interactive loop that it is central control for the program
+flag=True
+
+while flag:
+    userInput=input()
+    if (userInput=='help'):
+        pass
+        #getHelp() command to display information about each command
+
+    elif (userInput=='listtrees'):
+        #listTrees()
+    elif (userInput=='treeinfo'):
+        #treeInfo
+    elif (userInput=='nearby'):
+        #nearby()
+    elif (userInput=='quit'):
+        exit()
+    else:
+        print('Command not understood. Please enter valid command.')
