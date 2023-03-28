8fb85c16250699ea65c25fcf2a595d429e0cca85
Arishenryl <95382595+Arishenryl@users.noreply.github.com>
2023-03-28 17:15:18 -0400

    Update help.py

--- help.py
+++ help.py
@@ -1,7 +1,7 @@
-print ("list trees" can help one get a list of common tree names.)
+print ("listtrees: can help one get a list of common tree names.")
 
-print ("tree_info can get information about a species of tree. One input argument is required to run the code.")
+print ("treeinfo: can get information about a species of tree. One input argument is required to run the code.")
 
-print ("nearby can get a list of information of all trees within a defined distance from some location. Various input arguments are required to run the code.")
+print ("nearby: can get a list of information of all trees within a defined distance from some location. Various input arguments are required to run the code.")
 
-print ("The quit command quits the application.")
+print ("quit: command quits the application.")
