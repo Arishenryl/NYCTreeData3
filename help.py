5fe074164a9f20d58cd1dd9ab5961569fd8bb019
Arishenryl <95382595+Arishenryl@users.noreply.github.com>
2023-03-28 17:12:43 -0400

    Update help.py

--- help.py
+++ help.py
@@ -6,6 +6,6 @@ print ("tree_info can get information about a species of tree. One input argumen
 
 print ("nearby can get a list of information of all trees within a defined distance from some location. Various input arguments are required to run the code.")
 
-print ("The quit commandb quits the application.")
+print ("The quit command quits the application.")
 
 
