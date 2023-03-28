fd709a8fe0f6e044360e721e7a1238636b7f7b23
Arishenryl <95382595+Arishenryl@users.noreply.github.com>
2023-03-28 17:21:28 -0400

    Update help.py

--- help.py
+++ help.py
@@ -1,7 +1,8 @@
+#beginning of help command file; basically a "man" page
 print ("listtrees: can help one get a list of common tree names.")
-
+#treeinfo command help file description
 print ("treeinfo: can get information about a species of tree. One input argument is required to run the code.")
-
+#nearby command help  file description 
 print ("nearby: can get a list of information of all trees within a defined distance from some location. Various input arguments are required to run the code.")
-
+#how to quit the help command application
 print ("quit: command quits the application.")
