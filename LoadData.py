6bfa10bb438a2de90eac2d8dbebe18bce9c9393e
Arishenryl <95382595+Arishenryl@users.noreply.github.com>
2023-03-28 17:49:24 -0400

    Update LoadData.py

--- LoadData.py
+++ LoadData.py
@@ -11,7 +11,15 @@ Tree_data= pd.read_csv(file)
 
 #Functions
 def getHelp():
-    #Part 2 code here
+#beginning of help command file; basically a "man" page
+print ("listtrees: can help one get a list of common tree names.")
+#treeinfo command help file description
+print ("treeinfo: can get information about a species of tree. One input argument is required to run the code.")
+#nearby command help  file description 
+print ("nearby: can get a list of information of all trees within a defined distance from some location. Various input arguments are required to run the code.")
+#how to quit the help command application
+print ("quit: command quits the application.")
+
 def listTrees():
     #Part 3 code here
 def treeInfo():
