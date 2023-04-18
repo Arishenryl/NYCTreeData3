34c80d83e957c010ac5cb2c2d2db60b2b64790cf
aidalita <129125232+aidalita@users.noreply.github.com>
2023-04-18 16:35:42 -0400

    Update tree-info.py

--- tree-info.py
+++ tree-info.py
@@ -1,3 +1,6 @@
 data = read_data('nyctreedata.csv')
 print('Welcome to the treequery program.')
 print("To begin, try typing 'help' for the list of valid commands.\n")
+
+while true: 
+  command = input(' Enter a command: ')
