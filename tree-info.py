d2c31069f2c4872340b722cae9712fd94682a652
aidalita <129125232+aidalita@users.noreply.github.com>
2023-04-18 16:50:12 -0400

    Update tree-info.py

--- tree-info.py
+++ tree-info.py
@@ -4,3 +4,11 @@ print("To begin, try typing 'help' for the list of valid commands.\n")
 
 while true: 
   command = input(' Enter a command: ')
+  if command.lower() == 'help':
+        print("Valid commands:")
+        print("  treeinfo <tree_species_names>")
+        print("    - Find information about one or more tree species")
+        print("    - Example: treeinfo white oak")
+        print("  quit")
+        print("    - Quit the program")
+    elif command.lower() == 'quit':
