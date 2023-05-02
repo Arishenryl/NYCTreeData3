ac76fc5300b6b8868beca649d1288729432347b7
aidalita <129125232+aidalita@users.noreply.github.com>
2023-05-02 19:47:57 -0400

    Create quit.py

---
+++ quit.py
@@ -0,0 +1,8 @@
+elif command == 'quit':
+           print('\nGoodbye!\n')
+           break
+       # Invalid command.
+       else:
+           print('\nInvalid command. Please try again.\n')
+if __name__ == '__main__':
+   main()
