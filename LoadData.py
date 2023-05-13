fef55ff1c048a3022e98211c4c419922925d3a68
Arishenryl <95382595+Arishenryl@users.noreply.github.com>
2023-05-12 21:23:48 -0400

    Update LoadData.py

--- LoadData.py
+++ LoadData.py
@@ -61,6 +61,18 @@ def treeInfo(entry):
         else:
             continue
     freqa=freq(matched, boros)
+    
+  #Function which computes the distance between two points given their latitude and longitude
+def haversine(lat1, lon1, lat2, lon2):
+   R = 6371 # Radius of the earth in km
+   dLat = math.radians(lat2 - lat1)
+   dLon = math.radians(lon2 - lon1)
+   a = math.sin(dLat/2) * math.sin(dLat/2) +
+math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) *
+math.sin(dLon/2) * math.sin(dLon/2)
+   c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+   d = R * c # Distance in km
+   return d
 
 def frequencies(trees,boroughs):
     freq = {
