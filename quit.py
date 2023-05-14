12572623913b6ca8e72de8b0fe417839150a18ae
Arishenryl <95382595+Arishenryl@users.noreply.github.com>
2023-05-14 00:06:48 -0400

    Delete quit.py

--- quit.py
+++
@@ -1,8 +0,0 @@
-elif command == 'quit':
-           print('\nGoodbye!\n')
-           break
-       # Invalid command.
-       else:
-           print('\nInvalid command. Please try again.\n')
-if __name__ == '__main__':
-   main()
